b9571d30a8a95da56c2fcc30290243491f57c42d
salma1601 <salma1601@users.noreply.github.com>
2023-11-11 17:07:15 +0100

    Create test_grille_cellule.py

---
+++ test_grille_cellule.py
@@ -0,0 +1,8 @@
+from apply_life_rules import Grille, apply_life_rules
+
+
+def test_apply_life_rules():
+    grille = Grille(10)
+    grille.generer()
+    vivantes = apply_life_rules(grille, [])
+    assert(vivantes == [])
